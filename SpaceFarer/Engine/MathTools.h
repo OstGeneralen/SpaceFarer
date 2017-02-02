18dcc51009fd30fbad30e97d10dca9f1b170f033
Thejokemachine <joakimbensryd@gmail.com>
2017-02-02 22:24:44 +0100

    Math functions
    Randf - Returns a random value between 0 and 1.
    Dot - Returns the dot product between two Vector2fs.
    Length - Returns the length of a vector.
    Length2 - same but squared for optimization.
    Normalize - Normalizes a Vector2f.

--- SpaceFarer/Engine/MathTools.h
+++ SpaceFarer/Engine/MathTools.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <SFML\System\Vector2.hpp>
 #define MT_PI 3.141592f
 
 namespace MathTools
@@ -25,6 +26,34 @@ namespace MathTools
 		returnVector.x = static_cast<float>(aX + (aW / 2));
 		returnVector.y = static_cast<float>(aY + (aH / 2));
 		return returnVector;
+	}
+
+	static float Randf()
+	{
+		return abs(sinf(rand()));
+	}
+
+	static float Dot(const sf::Vector2f& aVector1, const sf::Vector2f& aVector2)
+	{
+		return aVector1.x * aVector2.x + aVector1.y * aVector2.y;
+	}
+
+	static float Length(const sf::Vector2f aVector)
+	{
+		return sqrt(aVector.x * aVector.x + aVector.y * aVector.y);
+	}
+
+	static float Length2(const sf::Vector2f aVector)
+	{
+		return aVector.x * aVector.x + aVector.y * aVector.y;
+	}
+
+	static void Normalize(sf::Vector2f& aVectorToNormalize)
+	{
+		sf::Vector2f tmpVector;
+		tmpVector.x = aVectorToNormalize.x / Length(aVectorToNormalize);
+		tmpVector.y = aVectorToNormalize.y / Length(aVectorToNormalize);
+		aVectorToNormalize = tmpVector;
 	}
 }
 
