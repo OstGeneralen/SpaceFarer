aa0eb82cb4c77fff8b1abafd4aca11647130dc29
Thejokemachine <joakimbensryd@gmail.com>
2017-02-23 22:24:02 +0100

    Refactoring
    Removed some unneccessary code

--- SpaceFarer/Game/Ship/ShipTypes.cpp
+++ SpaceFarer/Game/Ship/ShipTypes.cpp
@@ -21,14 +21,14 @@ void ShipTypes::CreateBlueprints(const std::string & aJsonPath)
 
 		ShipFittings& current =		myShipModels[i];
 		current.myName =			specificShipData["name"].get<std::string>();
-		current.myMaxHealth =		specificShipData["maxHealth"].get<int>();
-		current.myValue =			specificShipData["value"].get<int>();
-		current.myTurnSpeed =		specificShipData["turnSpeed"].get<int>();
-		current.myAcceleration =	specificShipData["acceleration"].get<int>();
-		current.myFuelUsage =		specificShipData["fuelUsage"].get<int>();
-		current.myFuelTank =		specificShipData["fuelTankCapacity"].get<int>();
-		current.myInertiaFactor =	specificShipData["inertiaFactor"].get<int>();
-		current.myHasInertia =		specificShipData["hasInertia"].get<bool>();
+		current.myMaxHealth =		specificShipData["maxHealth"];
+		current.myValue =			specificShipData["value"];
+		current.myTurnSpeed =		specificShipData["turnSpeed"];
+		current.myAcceleration =	specificShipData["acceleration"];
+		current.myFuelUsage =		specificShipData["fuelUsage"];
+		current.myFuelTank =		specificShipData["fuelTankCapacity"];
+		current.myInertiaFactor =	specificShipData["inertiaFactor"];
+		current.myHasInertia =		specificShipData["hasInertia"];
 		current.myWeaponType =		static_cast<WeaponTypes>(specificShipData["weaponType"].get<int>());
 		
 		current.myCurrentHealth = current.myMaxHealth;
